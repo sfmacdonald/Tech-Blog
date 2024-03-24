115eed48169564e16bde8696ca970beb7cbe23af
sfmacdonald <seanfmacdonald@icloud.com>
2024-03-24 11:40:44 -0700

    Change connection.js to use JAWSDB for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
 // /config/connection.js
 const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Make sure to require dotenv if you're using environment variables
+require('dotenv').config(); 
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL) // Use JAWSDB_URL for Heroku deployment with JawsDB MySQL
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL) // Use JAWSDB_URL for Heroku deployment with JawsDB MySQL
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
       dialect: 'mysql',
-      port: process.env.DB_PORT || 3306,
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-    });
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
