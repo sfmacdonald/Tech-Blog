4d99dfbd9986e0abe95f3d3476faddd0d25c5736
sfmacdonald <seanfmacdonald@icloud.com>
2024-03-27 07:57:16 -0700

    Change API route paths from lowercase to uppercase

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('/user', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
+router.use('/User', userRoutes);
+router.use('/Post', postRoutes);
+router.use('/Comment', commentRoutes);
 
 module.exports = router;
